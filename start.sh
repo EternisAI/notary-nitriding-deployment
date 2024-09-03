08ed36d75d13b55006b61fc10d9a40337925a1a7
AmirSaber Sharifi <inbox@saberistic.com>
2024-09-03 16:14:52 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nitriding -fqdn notary.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -useacme true &
+nitriding -fqdn notary.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -acme true &
 echo "[sh] Started nitriding."
 
 sleep 1
