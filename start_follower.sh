1bf25b0aa01e6903abd587b429b1fb477ad3dba8
AmirSaber Sharifi <inbox@saberistic.com>
2024-09-27 15:29:37 -0500

    ask nitriding to run notary when ready

--- start_follower.sh
+++ start_follower.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-nitriding -fqdn follower.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -wait-for-app &
+nitriding -fqdn follower.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -wait-for-app -appcmd "/app/notary-server --config-file /app/config/config_follower.yml"&
 echo "[sh] Started nitriding."
-
-sleep 1
-
-/app/notary-server --config-file /app/config/config_follower.yml
-echo "[sh] Notary server closed."
--- start_leader.sh
+++ start_leader.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-nitriding -fqdn leader.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 &
+nitriding -fqdn leader.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -appcmd "/app/notary-server --config-file /app/config/config_leader.yml" &
 echo "[sh] Started nitriding."
-
-sleep 60
-
-/app/notary-server --config-file /app/config/config_leader.yml
-echo "[sh] Notary server closed."
