4523619e56bdb6786f4f47d6f497a214762fd3f7
AmirSaber Sharifi <inbox@saberistic.com>
2024-09-27 16:58:20 -0500

    fix

--- start_leader.sh
+++ start_leader.sh
@@ -3,8 +3,16 @@
 nitriding -fqdn leader.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -wait-for-app &
 echo "[sh] Started nitriding."
 
-
-
+while true; do
+    response=$(curl -s -o /dev/null -w "%{http_code}" https://localhost/enclave/state -k)
+    if [ "$response" = "200" ]; then
+        echo "[sh] Nitriding is ready."
+        break
+    else
+        echo "[sh] Waiting for nitriding to be ready..."
+        sleep 5
+    fi
+done
 
 /app/notary-server --config-file /app/config/config_leader.yml
 echo "[sh] Started notary-server."
