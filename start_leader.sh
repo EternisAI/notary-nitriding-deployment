e19b5affb47fdb973eeebc7c4978d6172ed32bd6
AmirSaber Sharifi <inbox@saberistic.com>
2024-09-27 14:31:00 -0500

    fix

--- start_leader.sh
+++ start_leader.sh
@@ -3,7 +3,7 @@
 nitriding -fqdn leader.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 &
 echo "[sh] Started nitriding."
 
-sleep 1
+sleep 60
 
 /app/notary-server --config-file /app/config/config_leader.yml
 echo "[sh] Notary server closed."
