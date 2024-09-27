192a24cb8f200c788fa018fbb57f71b623c97604
AmirSaber Sharifi <inbox@saberistic.com>
2024-09-27 16:35:38 -0500

    check nitriding running before starting notary

--- start_follower.sh
+++ start_follower.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
-nitriding -fqdn follower.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -wait-for-app -appcmd "/app/notary-server --config-file /app/config/config_follower.yml"&
+nitriding -fqdn follower.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -wait-for-app 
 echo "[sh] Started nitriding."
+
+while true; do
+    response=$(curl -s -o /dev/null -w "%{http_code}" https://localhost/enclave/state -k)
+    if [ "$response" = "200" ]; then
+        echo "[sh] Nitriding is ready."
+        break
+    else
+        echo "[sh] Waiting for nitriding to be ready..."
+        sleep 5
+    fi
+done
+
+/app/notary-server --config-file /app/config/config_follower.yml
+echo "[sh] Started notary-server."
--- start_leader.sh
+++ start_leader.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-nitriding -fqdn leader.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -appcmd "/app/notary-server --config-file /app/config/config_leader.yml" &
+nitriding -fqdn leader.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -wait-for-app &
 echo "[sh] Started nitriding."
+
+
+
+
+/app/notary-server --config-file /app/config/config_leader.yml
+echo "[sh] Started notary-server."
