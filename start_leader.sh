94502dde41dc067627afd1ac1268046cf2512a9e
AmirSaber Sharifi <inbox@saberistic.com>
2024-09-27 13:33:41 -0500

    change leader nitriding script

--- start_leader.sh
+++ start_leader.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nitriding -fqdn leader.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -wait-for-app &
+nitriding -fqdn leader.cluster.eternis.ai -fqdn-leader localhost -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -wait-for-app &
 echo "[sh] Started nitriding."
 
 sleep 1
