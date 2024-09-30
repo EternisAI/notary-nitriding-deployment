c6586c20071ecdd2a099dc7753a31b04719a0ea7
AmirSaber Sharifi <inbox@saberistic.com>
2024-09-30 14:22:52 -0500

    don't wait for app

--- start_follower.sh
+++ start_follower.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nitriding -fqdn follower.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -wait-for-app 
+nitriding -fqdn follower.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 &
 echo "[sh] Started nitriding."
 
 while true; do
