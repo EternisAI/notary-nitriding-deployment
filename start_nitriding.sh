4028708deb408f6cd86105611a62ff0fba4bd07b
AmirSaber Sharifi <inbox@saberistic.com>
2024-10-01 15:34:55 -0500

    take debug out

--- start_nitriding.sh
+++ start_nitriding.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nitriding -fqdn follower.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 -debug &
+nitriding -fqdn follower.cluster.eternis.ai -fqdn-leader leader.cluster.eternis.ai -appwebsrv http://127.0.0.1:7047 -ext-pub-port 443 -intport 8080 &
 echo "[sh] Started nitriding."
 
 sleep 20;
